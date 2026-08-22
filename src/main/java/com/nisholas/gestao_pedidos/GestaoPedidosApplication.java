60915255a72383d0f78342f38b3dff9a317e7b3b
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-22 11:00:18 -0300

    feat: estrutura inicial do projeto

---
+++ src/main/java/com/nisholas/gestao_pedidos/GestaoPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.nisholas.gestao_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoPedidosApplication.class, args);
+	}
+
+}
