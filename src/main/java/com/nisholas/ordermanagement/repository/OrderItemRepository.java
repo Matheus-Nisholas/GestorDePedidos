56eaec2c04a74d0890c3f5733bfd71c6e7b43f92
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-23 18:05:21 -0300

    feat: adiciona repository de item de pedido

---
+++ src/main/java/com/nisholas/ordermanagement/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.nisholas.ordermanagement.repository;
+
+import com.nisholas.ordermanagement.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
