b7fca9ba7a57582442c266760a187d88225790ec
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-23 18:52:28 -0300

    test: usa profile de teste no context load

--- src/test/java/com/nisholas/ordermanagement/GestaoPedidosApplicationTests.java
+++ src/test/java/com/nisholas/ordermanagement/GestaoPedidosApplicationTests.java
@@ -2,12 +2,13 @@ package com.nisholas.ordermanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestaoPedidosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
