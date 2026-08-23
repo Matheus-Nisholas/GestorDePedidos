1c9c5e0734cc960e13918967d2f6e8102345177d
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-23 12:14:04 -0300

    refactor: simplifica response de produto

--- src/main/java/com/nisholas/ordermanagement/response/ProductResponse.java
+++ src/main/java/com/nisholas/ordermanagement/response/ProductResponse.java
@@ -2,15 +2,9 @@ package com.nisholas.ordermanagement.response;
 
 import lombok.Builder;
 
-import java.math.BigDecimal;
-
 @Builder
 public record ProductResponse(
         Long id,
-        String name,
-        String description,
-        BigDecimal price,
-        Integer stockQuantity,
-        Boolean active
+        String name
 ) {
 }
