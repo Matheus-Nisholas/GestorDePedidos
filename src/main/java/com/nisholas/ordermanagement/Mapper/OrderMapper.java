de70d26b68864c2626743909a13f7b26950ad3ec
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-23 14:40:17 -0300

    refactor: ajusta mapper para entidade Order

--- src/main/java/com/nisholas/ordermanagement/Mapper/OrderMapper.java
+++ src/main/java/com/nisholas/ordermanagement/Mapper/OrderMapper.java
@@ -1,6 +1,6 @@
 package com.nisholas.ordermanagement.Mapper;
 
-import com.nisholas.ordermanagement.entity.OrderItem;
+import com.nisholas.ordermanagement.entity.Order;
 import com.nisholas.ordermanagement.request.OrderRequest;
 import com.nisholas.ordermanagement.response.OrderResponse;
 import lombok.experimental.UtilityClass;
@@ -8,13 +8,13 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class OrderMapper {
 
-    public static OrderItem toOrder(OrderRequest orderRequest) {
-        OrderItem order = new OrderItem();
-        order.setStatus(orderRequest.status());
-        return order;
+    public static Order toOrder(OrderRequest orderRequest) {
+        return Order.builder()
+                .status(orderRequest.status())
+                .build();
     }
 
-    public static OrderResponse toOrderResponse(OrderItem order) {
+    public static OrderResponse toOrderResponse(Order order) {
         return OrderResponse.builder()
                 .id(order.getId())
                 .status(order.getStatus())
