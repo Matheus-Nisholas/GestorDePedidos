097981d0ab65f5acd9c046cf2bf5f94c298f8b6c
Matheus Nísholas <nisholas.workspace@outlook.com>
2026-08-24 12:37:48 -0300

    feat: configurar JWT no Swagger

--- src/main/java/com/nisholas/ordermanagement/config/OpenApiConfig.java
+++ src/main/java/com/nisholas/ordermanagement/config/OpenApiConfig.java
@@ -1,22 +1,38 @@
 package com.nisholas.ordermanagement.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
 
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
     @Bean
     public OpenAPI orderManagementOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("API de Gestão de Pedidos")
-                        .description("API REST para gerenciamento de clientes, produtos, pedidos e itens de pedido, com controle de estoque e regras de status do pedido.")
+                        .description("API REST para gerenciamento de clientes, produtos, pedidos e itens de pedido, com controle de estoque, regras de status e autenticação JWT.")
                         .version("v1")
                         .contact(new Contact()
-                                .name("Matheus Nísholas")));
+                                .name("Matheus Nísholas")))
+                .components(new Components()
+                        .addSecuritySchemes(
+                                SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                        ))
+                .addSecurityItem(
+                        new SecurityRequirement().addList(SECURITY_SCHEME_NAME)
+                );
     }
 }
